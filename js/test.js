7ff01e96babe6c8e99873c95932ddfa594ea7cc0
Dmytro <dimasurovatka22@gmail.com>
2023-02-09 01:17:57 +0100

    закончил на 3

---
+++ js/test.js
@@ -0,0 +1,58 @@
+// 1
+
+
+// const hours = 22;
+// const minutes = 0;
+
+// if (minutes === 0) {
+//   console.log(`${hours} ч.`)
+// } else {
+//   console.log(`${hours} ч. ${minutes} м.`)
+// }
+
+
+// 2
+
+// let userInput = prompt(`Введите число`);
+// console.log(userInput);
+// userInput = Number(userInput);
+
+// if (userInput > 0) {
+//   console.log(`Это положительное число`);
+// } else if (userInput === 0) {
+//   console.log('это ноль');
+// } else {
+//   console.log(`это отрицательное число`);
+// }
+
+
+// 3
+
+
+// const a = 40;
+// const b = 130;
+
+// if (a > 100 && b > 100) {
+//   if (a > b) {
+//     console.log(a)
+//   } else {
+//     console.log(b)
+//   }
+// } else {
+//   console.log(b + 512)
+// }
+
+// if (a > 100 && b > 100) {
+//   console.log(a > b ? a : b);
+// } else {
+//   console.log(b + 512);
+// }
+
+// if (a > 100 && b > 100) {
+//   console.log(Math.max(a, b));
+// } else {
+//   console.log(b + 512);
+// }
+
+// 4
+
