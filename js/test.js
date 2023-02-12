c06a669f042f10d0da5bfd8ccac68d18d66e8b52
Dmytro <dimasurovatka22@gmail.com>
2023-02-12 01:12:46 +0100

    9

--- js/test.js
+++ js/test.js
@@ -56,3 +56,85 @@
 
 // 4
 
+// let link = `htts://my-site.com/about`;
+
+// if (!link.endsWith(`/`)) {
+//   link += `/`;
+// }
+
+// console.log(link)
+
+
+// 5
+
+
+// let link = `htts://my-site.com/about`;
+
+// if (link.includes(`my-site`) && !link.endsWith(`/`)) {
+//   link += `/`;
+// }
+// console.log(link)
+
+
+// 6
+
+// больше либо равно 17, меньше или равно 24
+
+// const hours = 75;
+
+// if (hours < 17) {
+//   console.log(`Pedning`);
+// } else if (hours >= 17 && hours <= 24) {
+//   console.log(`Expires`);
+// } else {
+//   console.log(`Overdue`);
+// }
+
+
+// 7
+
+
+// const daysUntilDeadline = 0;
+
+// if (daysUntilDeadline === 0) {
+//   console.log(`Сегодня`);
+// } else if (daysUntilDeadline === 1) {
+//   console.log(`Завтра`);
+// } else if (daysUntilDeadline === 2) {
+//   console.log(`Послезавтра`);
+// } else {
+//   console.log(`Дата в будущем`)
+// }
+
+
+// 8
+
+
+// const daysUntilDeadline = 0;
+
+// switch (daysUntilDeadline) {
+//   case 0:
+//     console.log(`Сегодня`);
+//     break;
+//   case 1:
+//     console.log(`Завтра`);
+//     break;
+//   case 2:
+//     console.log(`Послезавтра`);
+//     break;
+//   default:
+//     console.log(`Дата в будущем`);
+// }
+
+
+// 9
+
+
+// const max = 97;
+// const min = 20;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
